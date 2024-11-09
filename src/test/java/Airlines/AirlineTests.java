c8208f3e25e9854310cd78a6e63821b9004a6c38
abilashchintalacheruvula <chabilash@gmail.com>
2024-11-09 00:37:48 -0500

    Reading baseURI from Json file

--- src/test/java/Airlines/AirlineTests.java
+++ src/test/java/Airlines/AirlineTests.java
@@ -1,28 +1,29 @@
 package Airlines;
 
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import utils.RestUtils;
+import restUtils.RestUtils;
+import utils.JsonUtils;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
 public class AirlineTests {
 
     @Test
-    public void createAirline(){
+    public void createAirline() throws IOException {
 
-        String baseURI = "https://api.instantwebtools.net/v1/airlines";
+        Map<String, Object> data = JsonUtils.getJsonDataAsMap("airlines/qa/airlinesApiData.json");
+
+        String baseURI = (String) data.get("createAirlineEndPoint");
 
 //        String reqPayload = Payloads.getCreateAirlinePayloadFromString("88a46ba0-7030-40a9-b0a0-3edd6aa11bb2","ABC Ailines","India",
 //                "https://upload.wikimedia.org/wikipedia/en/thumb/9/9b/ABC_Airways_Logo.svg/india.png",
 //                "From India","Mumbai, India","www.abcairways.com","1980");
 
-        Map<String, Object> reqPayload = Payloads.getCreateAirlinePayloadFromMap("88a46ba0-7030-40a9-b0a0-3edd6aa13579","ABC Ailines","India",
+        Map<String, Object> reqPayload = Payloads.getCreateAirlinePayloadFromMap("88a46ba0-7030-40a9-b0a0-3edd6aa13509","ABC Ailines","India",
                 "https://upload.wikimedia.org/wikipedia/en/thumb/9/9b/ABC_Airways_Logo.svg/india.png",
                 "From India","Mumbai, India","www.abcairways.com","1980");
 
